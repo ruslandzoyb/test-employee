166daff60336610a21dfc33fe39ff8924a4b6671
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-09-10 23:57:47 +0300

    Add dbcontext

---
+++ Data/EmployeeContext.cs
@@ -0,0 +1,25 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+   public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<EmployeePosition> EmployeePositions { get; set; }
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
